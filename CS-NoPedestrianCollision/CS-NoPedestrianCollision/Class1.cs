f632de4785e8f60e687ebd34f55899304a701c12
Joelminer <89350141+Joelminer-satisfactory@users.noreply.github.com>
2023-07-22 16:27:02 +0200

    initial commit

---
+++ CS-NoPedestrianCollision/CS-NoPedestrianCollision/Class1.cs
@@ -0,0 +1,57 @@
+﻿using ICities;
+using CitiesHarmony.API;
+using HarmonyLib;
+using System;
+
+namespace NoPedestrianCollision
+{
+    public class DisablePedestrianCheck : LoadingExtensionBase, IUserMod
+    {
+        public string Name { get { return "No Pedestrian Collision"; } }
+        public string Description { get { return "This mod disables the collision check for pedestrians, meaning they will drive through them without stopping"; } }
+        public void OnEnabled()
+        {
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+        public void OnDisabled()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+    }
+    public class Patcher
+    {
+        private const string HarmonyId = "yourname.YourModName";
+        private static bool patched = false;
+
+        public static void PatchAll()
+        {
+            if (patched) return;
+
+            patched = true;
+            var harmony = new Harmony(HarmonyId);
+            harmony.PatchAll(typeof(Patcher).Assembly);
+        }
+
+        public static void UnpatchAll()
+        {
+            if (!patched) return;
+
+            var harmony = new Harmony(HarmonyId);
+            harmony.UnpatchAll(HarmonyId);
+            patched = false;
+        }
+
+        [HarmonyPatch(typeof(CarAI), "CheckOtherVehicles")]
+        class MyPatch
+        {
+            public static void Prefix(ref int lodPhysics)
+            {
+                if (lodPhysics == 0)
+                {
+                    lodPhysics = (Int32)1;
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Cities Skylines: No Pedestrian Collision
+source code repo for the No Pedestrian Collision mod for cities skylines
+
+## Installing
+1. subscribe to [No Pedestrian Collision](https://steamcommunity.com/sharedfiles/filedetails/?id=3007539804) and [Harmony](https://steamcommunity.com/workshop/filedetails/?id=2040656402) on the steam workshop.
+2. enable both mods ingame.
+3. enjoy, your vehicles now no longer collide with pedestrians
